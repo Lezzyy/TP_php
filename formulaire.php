fa1b555e6ea74f3f74c3e8610406e6d13c839357
Laure-Anne Urbaniak <laureane.urbaniak@gmail.com>
2017-09-05 20:19:56 +0200

    add form

--- formulaire.php
+++ formulaire.php
@@ -36,6 +36,15 @@
       <p class="text-center">PLEASE ENTER CORRECT PASSWORD</p>
     </header>
 
+    <div class="container">
+      <form action="secret.php" method="post">
+        <input type="text" name="name">
+        <input type="text" name="password">
+        <input type="submit" name="OK" value="OK">
+      </form>
+    </div>
+
+
 
     <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
     <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.12.0.min.js"><\/script>')</script>
